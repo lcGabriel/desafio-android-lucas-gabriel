f2a02e16ac457fea7161ae09251c3c683276cc4f
lcGabriel <reidney.lg@gmail.com>
2020-04-21 00:40:40 -0300

    Desafio Marvel

---
+++ app/src/main/java/br/com/lgsouza/marvel/model/api/entity/Constants.kt
@@ -0,0 +1,4 @@
+package br.com.lgsouza.marvel.model.api.entity
+
+const val API_KEY = "3e7abd865e8b376a270ccbcc6264c5c6"
+const val PRIVATE_KEY = "0caf867538db0af57c659d6704801b54c61d4330"
---
+++ app/src/main/java/br/com/lgsouza/marvel/model/api/entity/Thumbnail.kt
@@ -0,0 +1,6 @@
+package br.com.lgsouza.marvel.model.api.entity
+
+data class Thumbnail(
+        val path: String,
+        val extension: String
+)
